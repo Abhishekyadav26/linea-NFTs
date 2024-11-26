1293ec336891f03e5eaf89a9cc74f84c4cfb5a2a
Abhishekyadav26 <aky732244@gmail.com>
2024-11-27 00:11:16 +0530

    initial commit

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,8 +1,6 @@
 import { createThirdwebClient } from "thirdweb";
 
-// Replace this with your client ID string
-// refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID as string;
 
 if (!clientId) {
   throw new Error("No client ID provided");
@@ -11,3 +9,5 @@ if (!clientId) {
 export const client = createThirdwebClient({
   clientId: clientId,
 });
+
+export const ContractAddress = process.env.NEXT_PUBLIC_TEMPLATE_Address_ID as string;
